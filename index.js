00ca731ccaa78c89f5ce434d809db4e877dfb746
Yahya <yahyahussein670@gmail.com>
2022-06-18 08:10:07 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+   for (let i = 0; i < array.length ; i++) {
+     for (let j = i + 1; j < array.length ; j++) {
+       if (array[i] + array[j] == target) return true;
 }
+   }
+return false;
+}
+console.log(hasTargetSum([1,2,5], 4))
+
 
 /* 
   Write the Big O time complexity of your function here
+     
+        0(n)
 */
+
 
 /* 
   Add your pseudocode here
+  1. function hasTargetSum(array, target)
+  2. for (let i = 0; i < array.length /2 for loop to loop over the arrays in the array parameter
+  3. array.length -1 -i (to get the last element in the array)
+  4. if(array[i] + array[j] == target) to return true if the first element and last element in the array sums up to the value of target
+  5. else to return false
 */
 
 /*
